ccc9b613e5d9d87e1875f9d3015e7e269b396c91
Vedant Pandey <vedantpandey46@gmail.com>
2020-09-20 13:33:04 +0530

    Implement trap signal codes

--- lc3vm.c
+++ lc3vm.c
@@ -79,6 +79,19 @@ enum
 };
 
 /**
+ * Set of available trap codes
+ */
+enum
+{
+	TRAP_GETC = 0x20,		/* Get character from keyboard, not echoed onto the terminal */
+	TRAP_OUT = 0x21,		/* Output a character */
+	TRAP_PUTS = 0x22,		/* Output a word string */
+	TRAP_IN = 0x23,			/* Get character from keyboard, echoed onto the terminal */
+	TRAP_PUTSP = 0x24,	/* Output a byte string */
+	TRAP_HALT = 0x25		/* Halt the program */
+};
+
+/**
  * Applying the necessary padding to the number
  * For negative sign the padding of 1 should be applied for maintaining the 2's complement
  */
@@ -321,9 +334,64 @@ int main(int argc, const char* argv[])
 
 				break;
 			case OP_TRAP:
-				/**
-				 * Operation yet to implement
-				 */
+				switch (INSTR & 0xFF)
+				{
+					case TRAP_GETC:
+						/* Read a single ASCII character */
+						REGISTER[R_R0] = (uint16_t)getchar();
+						break;
+					case TRAP_OUT:
+						/* Put a single ASCII character to standard output */
+						putc((char)REGISTER[R_R0], stdout);
+						fflush(stdout);
+						break;
+					case TRAP_PUTS:
+						{
+							uint16_t* c = memory + REGISTER[R_R0];
+							while (*c)
+							{
+								putc((char)*c, stdout);
+								++c;
+							}
+							fflush(stdout);
+						}
+
+						break;
+					case TRAP_IN:
+						{
+							printf("Enter a character: ");
+							char c = getchar();
+							putc(c, stdout);
+							REGISTER[R_R0] = (uint16_t)c;
+						}
+
+						break;
+					case TRAP_PUTSP:
+						{
+							/**
+							 * One character per byte(two bytes per word)
+							 * Here we need to swap back to big endian format
+							 */
+							uint16_t* c = memory + REGISTER[R_R0];
+							while (*c)
+							{
+								char char1 = (*c) & 0xFF;
+								putc(char1, stdout);
+								char char2 = (*c) >> 8;
+								if (char2) putc(char2, stdout);
+								++c;
+							}
+							fflush(stdout);
+						}
+
+						break;
+					case TRAP_HALT:
+						puts("HALT");
+						fflush(stdout);
+						running = 0;
+						break;
+				}
+
 				break;
 			case OP_RES:
 			case OP_RTI:
