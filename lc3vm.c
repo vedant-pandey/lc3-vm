7b478532eea44f032caafb796542c1b101362111
Vedant Pandey <vedantpandey46@gmail.com>
2020-09-20 14:51:29 +0530

    Add image read procedure

--- lc3vm.c
+++ lc3vm.c
@@ -117,7 +117,40 @@ void update_flags(uint16_t r) {
 	{
 		REGISTER[R_COND] = FL_POS;
 	}
+}
 
+/* Swap */
+uint16_t swap16(uint16_t x)
+{
+	return (x << 8) | (x >> 8);
+}
+
+void read_image_file(FILE* file)
+{
+	/* Origin tells the location to store the image in memory */
+	uint16_t origin;
+	fread(&origin, sizeof(origin), 1, file);
+	origin = swap16(origin);
+
+	uint16_t max_read = UINT16_MAX - origin;
+	uint16_t* p = memory + origin;
+	size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+	/* Swap to little endian */
+	while (read-- > 0)
+	{
+		*p = swap16(*p);
+		++p;
+	}
+}
+
+int read_image(const char* image_path)
+{
+	FILE* file = fopen(image_path, "rb");
+	if (!file) return 0;
+	read_image_file(file);
+	fclose(file);
+	return 1;
 }
 
 int main(int argc, const char* argv[])
